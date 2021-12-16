6d58d42fa3eeb96e605c718fa3a08ec73019bd0b
Aysen23 <borisov_aisen@mail.ru>
2021-12-16 20:25:43 +0900

    добавили шаблонизацию и поработали над шапкой

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index_page():
-    return render_template("index.html")
+    return render_template("home.html")
+
+@app.route("/product")
+def product_page():
+    return render_template("product.html")
 
 # точка входа
 if __name__ == "__main__":
